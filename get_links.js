471ef06f861908d3f38f66d209b2883fe3050c38
sam <sam@motherapp.com>
2016-01-13 18:00:33 +0800

    fix extra url grep

--- get_links.js
+++ get_links.js
@@ -6,7 +6,7 @@ for (var i=0; i<tempDOM.length; i++) {
     for (var j=0; j<aDOM.length; j++) {
       var url = aDOM[j].href;
       if (url !== 'null' && url !== null && url !== undefined) {
-        var isChangeset = url.indexOf('changeset') > 0;
+        var isChangeset = url.indexOf('/changeset/') > 0;
         if (isChangeset) {
           aLink.push(url);
         }
